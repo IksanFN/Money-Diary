ffc38880bd778c9745c65ec23327c7ee7119bba7
IksanFN <iksanfauzi727@gmail.com>
2023-08-22 12:08:20 +0700

    User Create

--- app/Http/Controllers/Main/UserController.php
+++ app/Http/Controllers/Main/UserController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Main;
 
-use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Tables\Users;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserStore;
 use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use ProtoneMedia\Splade\Facades\Toast;
-use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
@@ -20,25 +22,27 @@ class UserController extends Controller
 
     public function create()
     {
-        $roles = Role::all();
+        $roles = Role::whereNot('name', 'admin')->get();
         return view('main.users.create', compact('roles'));
     }
 
-    public function store(Request $request)
+    public function store(UserStore $request)
     {
-        $this->validate($request, [
-            'name' => 'required',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:6',
-            'roles' => 'required',
-        ]);
+        // File avatar
+        $image = $request->file('avatar');
+        $nameImage = Str::random(32).$request->avatar->getClientOriginalName();
         // return dd($request->all());
+        $image->storeAs('public/users/avatar', $nameImage);
 
+        
         $user = User::create([
             'name' => $request->name,
+            'nisn' => $request->nisn,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'avatar' => $nameImage,
         ]);
+
         $user->assignRole($request->roles);
 
         Toast::title('Users berhasil dibuat')->backdrop()->center()->autoDismiss(1);
@@ -52,7 +56,7 @@ class UserController extends Controller
 
     public function edit(Request $request, User $user)
     {
-        $roles = Role::all();
+        $roles = Role::whereNot('name', 'admin')->get();
         $userRole = $user->roles;
         return view('main.users.edit', compact('user', 'roles', 'userRole'));
     }
